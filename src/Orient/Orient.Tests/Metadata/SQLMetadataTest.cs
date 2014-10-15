29e4e672a3ab9340743e8541cd91ba56a2a333de
Roman Gordon <goormoon@gmail.com>
2014-10-15 11:09:07 +0300

    Added tests for metadata

---
+++ src/Orient/Orient.Tests/Metadata/SQLMetadataTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orient.Client;
+
+namespace Orient.Tests.Metadata
+{
+    [TestClass]
+    public class SQLMetadataTest
+    {
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void QuerySchemaClasses()
+        {
+            using (var context = new TestDatabaseContext())
+            using(var database = new ODatabase(TestConnection.GlobalTestDatabaseAlias))
+            {
+                var document = database
+                    .Query("select expand(classes) from metadata:schema");
+                Assert.IsTrue(document.Count > 0); 
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void QuerySchemaProperties()
+        {
+            using (var context = new TestDatabaseContext())
+            using(var database = new ODatabase(TestConnection.GlobalTestDatabaseAlias))
+            {
+                var document = database
+                    .Query("select expand(properties) from (select expand(classes) from metadata:schema) where name = 'OUser'");
+                Assert.IsTrue(document.Count > 0); 
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void QueryIndexes()
+        {
+            using (var context = new TestDatabaseContext())
+            using(var database = new ODatabase(TestConnection.GlobalTestDatabaseAlias))
+            {
+                var document = database
+                    .Query("select expand(indexes) from metadata:indexmanager");
+                Assert.IsTrue(document.Count > 0); 
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Metadata")]
+        [ExpectedException(typeof(OException))]
+        public void QueryMetadataNotSupported()
+        {
+            using (var context = new TestDatabaseContext())
+            using(var database = new ODatabase(TestConnection.GlobalTestDatabaseAlias))
+            {
+                var document = database
+                    .Query("select expand(indexes) from metadata:blaaa");
+            }
+        }
+    }
+}
